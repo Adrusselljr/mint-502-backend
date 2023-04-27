f92352286d37cbf0dc3a39b02c5450f6999f8e75
Aaron Russell <cpl.adrusselljr@gmail.com>
2023-04-27 12:03:39 -0400

    initial setup

---
+++ server/app.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const cors = require('cors');
+var logger = require('morgan');
+const mongoose = require("mongoose");
+
+mongoose
+  .set("strictQuery", false)
+  .set("strictPopulate", false)
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Established a connection to the database'))
+  .catch(err => console.log('Something went wrong when connecting to the database ', err))
+
+  const indexRouter = require('./routes/index')
+  const userRouter = require('./routes/user/userRouter')
+  const productRouter = require('./routes/product/productRouter')
+  const cartRouter = require('./routes/cart/cartRouter')
+  const sellerRouter = require('./routes/seller/sellerRouter')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter)
+app.use('/user', userRouter)
+app.use('/product', productRouter)
+app.use('/cart', cartRouter)
+app.use('/seller', sellerRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
